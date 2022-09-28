dd28cb7ed80808f754a71daa3787899d7dc4364d
Gilmar Alves <gilmaralves914@gmail.com>
2022-09-27 22:58:06 -0300

    Melhorando as respostas

--- js/cadastroD.js
+++ js/cadastroD.js
@@ -1,9 +1,17 @@
 const tabela = document.getElementById('tabela');
 const botaoSalvar = document.getElementById('botaosalvar');
+const resultado = document.getElementById('result');
+
+
+
 tabela.style.display = 'none'
 botaoSalvar.style.display = 'none'
 function AbrirConsulta() {
     tabela.style.display = 'block'
+    botaoSalvar.style.display = 'none'
+    inputCod.style.display = 'none'
+    inputDes.style.display = 'none'
+    resultado.innerHTML = '';
 
 }
 function IncluirConsulta() {
@@ -11,27 +19,63 @@ function IncluirConsulta() {
     tabela.style.display = 'block'
     botaoSalvar.style.display = 'block'
 
-    var corpoTabelaCodigo = document.querySelector('#codigo');
-    var corpoTabelaDes = document.querySelector('#descricao');
+    const corpoTabela = document.querySelector('#tbody');
 
-    corpoTabelaCodigo.innerHTML = "<input>";
-    corpoTabelaDes.innerHTML = "<input  style='width:90%'>";
+    corpoTabela.innerHTML =
+        `<tr>
+    <td id='codigo'></td>
+    <td id='descricao'></td>
+    </tr>`
+    const corpoCod = document.querySelector('#codigo');
+    const corpoDes = document.querySelector('#descricao');
 
+    corpoCod.innerHTML = "<input id='inputCod'>";
+    corpoDes.innerHTML = "<input id='inputDes' style='width:100%'>";
+    const inputCod = document.querySelector('#inputCod');
+    const inputDes = document.querySelector('#inputDes');
 
-    botaoSalvar.addEventListener("click", () => {
+    resultado.innerHTML = '';
 
-        alert('salvo')
+    botaoSalvar.innerHTML = 'Adicionar'
+    botaoSalvar.addEventListener("click", (e) => {
+        e.preventDefault();
+        resultado.innerHTML = 'Adicionado com sucesso';
+
+        inputCod.value = '';
+        inputDes.value = '';
     })
-
 
 }
 
+
+
+function AlterarConsulta() {
+    botaoSalvar.style.display = 'block'
+    botaoSalvar.innerHTML = 'Alterar'
+    tabela.style.display = 'block'
+    inputCod.style.display = 'none'
+    inputDes.style.display = 'none'
+    resultado.innerHTML = '';
+    botaoSalvar.addEventListener("click", (e) => {
+        e.preventDefault();
+        resultado.innerHTML = 'Alterado com sucesso';
+
+    })
+
+}
 function ExcluirConsulta() {
     botaoSalvar.style.display = 'block'
     tabela.style.display = 'block'
-    botaoSalvar.addEventListener("click", () => {
+    resultado.innerHTML = '';
+    inputCod.style.display = 'none'
+    inputDes.style.display = 'none'
+    botaoSalvar.innerHTML = 'Excluir'
+    botaoSalvar.addEventListener("click", (e) => {
+        e.preventDefault();
+        resultado.innerHTML = 'Excluido com sucesso';
 
-        alert('Excluido com sucesso')
     })
 
 }
+
+
